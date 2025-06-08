58df4acf1ca02cc79a9a9f7e6ffb12ceaf02b181
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-06-08 14:51:47 -0500

    testing page change function

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 
 const vsCpuBtn = document.querySelector("#vs-cpu");
 const vsPlayerBtn = document.querySelector("#vs-player");
+const selectX = document.querySelector("#select-x");
+const selectO = document.querySelector("#select-o");
+const main = document.querySelector("main");
+
+
+// vsCpuBtn.addEventListener("click", displayPage("solo"));
+
+// async function displayPage(page) {
+//     let url = `${page}.html`
+//     try {
+//         const response = await fetch(url);
+//         if (!response.ok) {
+//             throw new Error("Page load error: " + response.status)
+//         }
+//         const data = await response.text();
+//         main.innerHTML = data;
+//     } catch(error) {
+//         alert(error);
+//     }
+
+// }
