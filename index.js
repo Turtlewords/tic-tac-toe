cec3044ab34262021544e25d35d2cfce9ec0467e
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-06-09 16:25:38 -0500

    begin working on solo game page

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ const selectO = document.querySelector("#select-o");
 const main = document.querySelector("main");
 
 
-// vsCpuBtn.addEventListener("click", displayPage("solo"));
+vsCpuBtn.addEventListener("click", () => {
+    displayPage("solo");
+});
 
-// async function displayPage(page) {
-//     let url = `${page}.html`
-//     try {
-//         const response = await fetch(url);
-//         if (!response.ok) {
-//             throw new Error("Page load error: " + response.status)
-//         }
-//         const data = await response.text();
-//         main.innerHTML = data;
-//     } catch(error) {
-//         alert(error);
-//     }
 
-// }
+async function displayPage(page) {
+    let url = `${page}.html`
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Page load error: " + response.status)
+        }
+        const data = await response.text();
+        main.innerHTML = data;
+    } catch(error) {
+        alert(error);
+    }
+
+}
